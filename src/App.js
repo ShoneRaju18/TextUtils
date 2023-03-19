40163d6534b13d8c5651658eb5e5fd9c543ac82c
Shone Raju <VE00YM066@globalymc.com>
2023-03-19 11:48:20 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Shone
         </a>
       </header>
     </div>
